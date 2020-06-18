ec428595f0272312d6b329581b577fc3b39ae0a3
Angelo Medeiros <angelo.nobrega@envolti.com.br>
2020-06-18 15:42:20 -0300

    Firefox

--- wdio.conf.js
+++ wdio.conf.js
@@ -46,20 +46,27 @@ exports.config = {
   // https://docs.saucelabs.com/reference/platforms-configurator
   //
   capabilities: [
+    // {
+    //   // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+    //   // grid with only 5 firefox instances available you can make sure that not more than
+    //   // 5 instances get started at a time.
+    //   maxInstances: 5,
+    //   //
+    //   browserName: 'chrome',
+    //   'goog:chromeOptions': {
+    //     args: ['--headless', '--disabled-gpu'],
+    //   },
+    //   // If outputDir is provided WebdriverIO can capture driver session logs
+    //   // it is possible to configure which logTypes to include/exclude.
+    //   // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+    //   // excludeDriverLogs: ['bugreport', 'server'],
+    // },
     {
-      // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-      // grid with only 5 firefox instances available you can make sure that not more than
-      // 5 instances get started at a time.
       maxInstances: 5,
-      //
-      browserName: 'chrome',
-      'goog:chromeOptions': {
-        args: ['--headless', '--disabled-gpu'],
+      browserName: 'firefox',
+      'moz:firefoxOptions': {
+        args: ['-headless'],
       },
-      // If outputDir is provided WebdriverIO can capture driver session logs
-      // it is possible to configure which logTypes to include/exclude.
-      // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-      // excludeDriverLogs: ['bugreport', 'server'],
     },
   ],
   //
